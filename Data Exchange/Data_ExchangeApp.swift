5ee281d00155200935e1ff58e9f28153a357048c
mikehallCD <mike@chipmunkdata.co.uk>
2024-10-25 09:36:43 +0100

    Initial Commit

---
+++ Data Exchange/Data_ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_ExchangeApp.swift
+//  Data Exchange
+//
+//  Created by Mike Hall on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Data_ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
